29e7463a2a59d9061a4c2db17b6b3b8827e5db24
Saifahmeed <saif_ahmed@aucegypt.edu>
2025-02-19 01:05:47 +0200

    1.1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,46 @@
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <p>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default App;
